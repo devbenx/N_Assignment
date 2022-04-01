bee209b0f1007d36ef21ea8dac038ea1cc35f716
Frank Benjamin <benjaminarias3197@gmail.com>
2022-04-01 13:49:55 +0200

    changes

--- index.js
+++ index.js
@@ -1,44 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+
+// API Points
 const product = require("./api/product");
 const cross_selling_products = require("./api/crosssellingproducts");
 
 const app = express();
 app.use(cors());
 
-const requestEndpoint = "http://dump.dataplatform.shoes/20201005_frontend_assignment/prod_details_362950.json";
-const requestEndPointCrossProducts = "http://dump.dataplatform.shoes/20201005_frontend_assignment/cross_sell_products_for_362950.json";
-
-const getProducts = () => {
-        return axios.get(requestEndpoint)
-                .then(response => {
-                        console.log(response.data)
-                        return response.data
-                }).catch(err => err);
-}
-
-const getCrossSellingProducts = () => {
-    return axios.get(requestEndPointCrossProducts)
-            .then(response => {
-                    console.log(response.data)
-                    return response.data
-            }).catch(err => err);
-}
-
-// This function runs if the http://localhost:5000/getData endpoint is requested with a GET request
-//  
-
-app.get('/CrossSellingProducts', async (req, res) => {
-    const response = await getCrossSellingProducts();
-    console.log(res.json(response));
-});
-
 app.use("/api/product", product);
 app.use("/api/crosssellingproducts", cross_selling_products);
 
-const port = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8090;
 
-app.listen(port, () => console.log(`Server Running on port ${port}`));
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
 
 module.exports = app;
